d6738cd53e920e2ca3eee8b4ead1ad95773c1a0c
Muhammad Henry Aditya <adityahenry2@gmail.com>
2024-09-26 09:42:53 +0800

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pengolahan_citra1/",
   plugins: [
     vue(),
     vueJsx(),
@@ -16,5 +17,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+    
   }
 })
