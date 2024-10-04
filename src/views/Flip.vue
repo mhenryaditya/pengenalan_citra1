ebd8240f3557d045561c28578b914c737b2159bd
MaulanaAnsari <maulana7104@gmail.com>
2024-10-04 10:35:35 +0800

    EC-19 feat: add flip featue

--- src/views/Flip.vue
+++ src/views/Flip.vue
@@ -1,8 +1,109 @@
 <template>
-    <div>
+    <div v-if="loading" class="mt-5 p-5 d-flex justify-content-center">
+         <div class="align-self-center p-5 shadow-lg rounded-lg">
+             <PulseLoader :size="'50px'"></PulseLoader>
+             <div class="mt-2 d-block d-flex flex-column gap-2 align-items-center">
+                 <h5>Loading...</h5>
+                 <span>Mohon Tunggu Sebentar</span>
+             </div>
+         </div>
+     </div>
+     <div v-else class="mt-5 pt-5">
+         <section class="section-shaped my-0">
+             <div class="container shape-container">
+                 <!-- Title -->
+                 <h3>Rotasi Gambar</h3>
+                 <!-- Image Preview -->
+                 <div class="row mt-3">
+                     <div class="col-6 d-flex flex-column align-items-center">
+                         <img :src="urlInit" alt="initPct" class="img-thumbnail">
+                         <span class="d-block text-center">Gambar Awal</span>
+                     </div>
+                     <div class="col-6 d-flex flex-column align-items-center">
+                         <img :src="urlAfter" alt="afterPct" class="img-thumbnail">
+                         <span class="d-block text-center">Gambar Akhir</span>
+                     </div>
+ 
+                 </div>
+                 <!-- Adjust Tools -->
+                 <div class="row mt-3">
+                     <!-- Tools -->
+                     <div class="col-6">
+                        <div>
+                        <base-button tag="a" type="primary" size="lg" href="#" class="active" @click="applyButtonHorizontal" role="button" :aria-pressed="horizontalPressed">Horizontal</base-button>
+                         <base-button tag="a" type="primary" size="lg" href="#" class="active" @click="applyButtonVertikal" role="button" :aria-pressed="vertikalPressed">Vertikal</base-button>
+                        </div>
+                         <span class="text-info d-block" style="font-size: 10pt;">Tekan Enter untuk menerapkan
+                             hasil</span>
+                     </div>
+                     <!-- Download Hasil -->
+                     <div class="col-6 d-flex flex-column flex-end">
+                         <base-button tag="a" :href="urlAfter" class="mb-3 mb-sm-0" type="success"
+                             icon="ni ni-cloud-download-95" download="rotated-image">
+                             Download Hasil
+                         </base-button>
+                     </div>
+                 </div>
+             </div>
+         </section>
+     </div>
+ </template>
+ 
+ <script setup lang="ts">
+ import router from '@/router';
+ // import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+ // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+ import BaseButton from '@/components/BaseButton.vue';
+ import { inject, onMounted, reactive, ref, watch, type Ref } from 'vue';
+ import PulseLoader from 'vue-spinner/src/ClipLoader.vue';
+ import { bufferImgUrl, createImgUrl } from '@/plugins/Image';
+ import VueAutoNumeric from '@/components/VueAutoNumeric.vue';
+ import { Jimp } from 'jimp';
+import type { RefSymbol } from '@vue/reactivity';
+ 
+ let initPct = inject<Ref<File | undefined>>('initPct');
+ let loading: Ref<boolean> = ref(true)
+ let urlInit: string
+ let urlAfter: string
+ let horizontalPressed: Ref<boolean> = ref(false)
+ let vertikalPressed: Ref<boolean> = ref(false)
+ 
+ if (!initPct!.value) {
+     router.push('/') // apabila user belum input gambar dari landing, maka arahkan ke landing
+ }
 
-    </div>
-</template>
+ onMounted(async () => {
+     await applyImg()
+ })
 
-<script setup lang="ts">
-</script>
+ let applyButtonHorizontal = async () => {
+    console.log("ok");
+     horizontalPressed.value=!horizontalPressed.value
+     await applyImg()
+ }
+
+ let applyButtonVertikal = () => {
+    vertikalPressed.value=!vertikalPressed.value
+}
+
+//  watch(() => horizontalPressed.value, async () => {
+//     await applyImg()
+//  })
+ 
+//  watch(() => vertikalPressed.value, async () => {
+//     await applyImg()
+//  })
+
+ let applyImg = async () => {
+     loading.value = true
+     urlInit = createImgUrl(initPct!.value as File)
+     let image = await Jimp.read(urlInit);
+    //  image.flip({horizontal: horizontalPressed.value, vertical: vertikalPressed.value});
+    // image.flip(options: );
+     
+     urlAfter = bufferImgUrl((await image.getBuffer(initPct!.value!.type as string)).buffer, `${initPct!.value?.name}-thresholding`, initPct!.value!.type as string)
+     loading.value = !loading.value
+ 
+     // image.brightness()
+ }
+ </script>
